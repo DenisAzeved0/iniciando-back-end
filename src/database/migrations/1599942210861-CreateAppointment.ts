95e95b4c782256fcb09916be4e836ac5aefecc48
Dênis Azevedo <denis.viegas.azevedo@gmail.com>
2020-10-05 22:08:53 -0300

    Initial commit (Banco de dados): - Configurando TypeORM - Tabela de agendamentos - Modelo de agendamento - Repositório do TypeORM

---
+++ src/database/migrations/1599942210861-CreateAppointment.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointment1599942210861
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isNullable: false,
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'datetime',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
