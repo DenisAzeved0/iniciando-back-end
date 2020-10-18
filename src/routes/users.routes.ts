504a01a65f34ec20d8b90ea5117ba812f14b8739
Dênis Azevedo <denis.viegas.azevedo@gmail.com>
2020-10-17 21:33:19 -0300

    Atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -24,8 +25,8 @@ usersRouter.post('/', async (request, response) => {
     delete user.password;
 
     return response.json(user);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
 });
 
@@ -34,9 +35,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
